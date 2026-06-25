737bd2f2241e71a1d3a72f8be060867188175c5f
Mohamed Mourad <mohamedmoradmagdy1000@gmail.com>
2026-06-25 19:45:05 +0300

    feat: read texts from disk.

--- tracking/analysis_results.py
+++ tracking/analysis_results.py
@@ -6,35 +6,27 @@ from lib.test.analysis.plot_results import plot_results, print_results, print_pe
 from lib.test.evaluation import get_dataset, trackerlist
 
 trackers = []
-dataset_name = 'lasot'
-"""stark"""
-# trackers.extend(trackerlist(name='stark_s', parameter_name='baseline', dataset_name=dataset_name,
-#                             run_ids=None, display_name='STARK-S50'))
-# trackers.extend(trackerlist(name='stark_st', parameter_name='baseline', dataset_name=dataset_name,
-#                             run_ids=None, display_name='STARK-ST50'))
-# trackers.extend(trackerlist(name='stark_st', parameter_name='baseline_R101', dataset_name=dataset_name,
-#                             run_ids=None, display_name='STARK-ST101'))
-"""TransT"""
-# trackers.extend(trackerlist(name='TransT_N2', parameter_name=None, dataset_name=None,
-#                             run_ids=None, display_name='TransT_N2', result_only=True))
-# trackers.extend(trackerlist(name='TransT_N4', parameter_name=None, dataset_name=None,
-#                             run_ids=None, display_name='TransT_N4', result_only=True))
-"""pytracking"""
-# trackers.extend(trackerlist('atom', 'default', None, range(0,5), 'ATOM'))
-# trackers.extend(trackerlist('dimp', 'dimp18', None, range(0,5), 'DiMP18'))
-# trackers.extend(trackerlist('dimp', 'dimp50', None, range(0,5), 'DiMP50'))
-# trackers.extend(trackerlist('dimp', 'prdimp18', None, range(0,5), 'PrDiMP18'))
-# trackers.extend(trackerlist('dimp', 'prdimp50', None, range(0,5), 'PrDiMP50'))
-"""ostrack"""
-trackers.extend(trackerlist(name='ostrack', parameter_name='vitb_256_mae_ce_32x4_ep300', dataset_name=dataset_name,
-                            run_ids=None, display_name='OSTrack256'))
-trackers.extend(trackerlist(name='ostrack', parameter_name='vitb_384_mae_ce_32x4_ep300', dataset_name=dataset_name,
-                            run_ids=None, display_name='OSTrack384'))
+dataset_name = 'mtc_aic4'
 
+# 1. Load the protected Kalman run
+trackers.extend(trackerlist(
+    name='abavitrack',
+    parameter_name='abavit_kalman',
+    dataset_name=dataset_name,
+    run_ids=None,
+    display_name='AbaViTrack (Kalman + Gamma)'
+))
+
+# 2. Load the new Baseline run
+trackers.extend(trackerlist(
+    name='abavitrack',
+    parameter_name='abavit_patch16_224',
+    dataset_name=dataset_name,
+    run_ids=None,
+    display_name='AbaViTrack (Baseline)'
+))
 
 dataset = get_dataset(dataset_name)
-# dataset = get_dataset('otb', 'nfs', 'uav', 'tc128ce')
-# plot_results(trackers, dataset, 'OTB2015', merge_results=True, plot_types=('success', 'norm_prec'),
-#              skip_missing_seq=False, force_evaluation=True, plot_bin_gap=0.05)
+
+# 3. Print the comparison table and plot the curves!
 print_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec', 'prec'))
-# print_results(trackers, dataset, 'UNO', merge_results=True, plot_types=('success', 'prec'))
